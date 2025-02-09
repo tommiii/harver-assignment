4dd6774f03f9e80015206c68d8cd5d48b4a1fdca
tommi <zagnitommaso@gmail.com>
2025-02-09 14:15:34 +0100

    chore: remove deprecated Redux store files for candidate matcher

--- client/src/store/root_reducer.ts
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "@reduxjs/toolkit";
-import candidate_matcher_reducer from "./slices/candidate_matcher_slice";
-
-export const root_reducer = combineReducers({
-  candidate_matcher: candidate_matcher_reducer,
-});
